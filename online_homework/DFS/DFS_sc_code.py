724797e7527fea7d672a6cdf400c7cd8ca902d54
Yu-jiin <jiin4083@naver.com>
2024-08-22 16:54:18 +0900

    0822 done

--- online_homework/DFS/DFS_sc_code.py
+++ online_homework/DFS/DFS_sc_code.py
@@ -1,2 +1,70 @@
-# 7 8
-# 1 2 1 3 2 4 2 5 4 6 5 6 6 7 3 7
+'''
+1
+7 8
+1 2 1 3 2 4 2 5 4 6 5 6 6 7 3 7
+'''
+def DFS(s, V):              # s시작정점, V 정점개수(1번부터인 정점의 마지막정점)
+    visited = [0]*(V+1)     # 방문한 정점을 표시
+    stack = []              # 스택생성
+    print(s)
+    visited[s] = 1          # 시작정점 방문표시
+    v = s
+    while True:
+        for w in adjL[v]:       # v에 인접하고, 방문안한 w가 있으면
+            if visited[w] == 0:
+                stack.append(v)  # push(v) 현재 정점을 push하고
+                v = w            # w에 방문
+                print(v)
+                visited[w] = 1     # w에 방문 표시
+                break           # for w, v부터 다시 탐색
+        else:                   # 남은 인접정점이 없어서 break가 걸리지 않은경우
+            if stack:   # 이전 갈림길을 스택에서 꺼내서... if TOP > -1
+                v = stack.pop()
+            else:       # 되돌아갈 곳이 없으면 남은 갈림길이 없으면 탐색종료
+                break   # while True:
+
+
+
+T = int(input())
+for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    adjL = [[] for _ in range(V+1)]
+    arr = list(map(int, input().split()))
+    for i in range(E):
+        v1, v2 = arr[i*2], arr[i*2+1]
+        adjL[v1].append(v2)
+        adjL[v2].append(v1)
+
+    DFS(1, V)
+
+
+
+
+'''
+7 8
+4 2 1 2 1 3 5 2 4 6 5 6 6 7 3 7
+'''
+
+def bfs(s, V):  # 시작정점 s, 마지막 정점 V
+    visited = [0] * (V+1)   # visited 생성
+    q = []          # 큐 생성
+    q.append(s)     # 시작점 인큐
+    visited[s] = 1  # 시작점 방문표시
+    while q:        # 큐에 정점이 남아있으면 front != rear
+        t = q.pop(0)    # 디큐
+        print(t)        # 방문한 정점에서 할일
+        for w in adj_l[t]:  # 인접한 정점 중 인큐되지 않은 정점 w가 있으면
+            if visited[w]==0:
+                q.append(w)     # w인큐, 인큐되었음을 표시
+                visited[w] = visited[t] + 1
+
+V, E = map(int, input().split()) # 1번부터 V번 정점, E개의 간선
+arr = list(map(int, input().split()))
+# 인접리스트 -------------------------
+adj_l = [[] for _ in range(V+1)]
+for i in range(E):
+    v1, v2 = arr[i*2], arr[i*2+1]
+    adj_l[v1].append(v2)
+    adj_l[v2].append(v1)    # 방향이 없는 경우
+# 여기까지 인접리스트 -----------------
+bfs(1, 7)
